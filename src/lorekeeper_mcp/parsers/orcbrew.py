eea3fbe449ed7382e4629f2dffa85e47ad4b92f8
Joe Maples <joe@maples.dev>
2025-11-14 10:35:50 -0500

    feat(parsers): create parser module structure

---
+++ src/lorekeeper_mcp/parsers/orcbrew.py
@@ -0,0 +1,9 @@
+"""OrcBrew (.orcbrew) file parser for D&D 5e content.
+
+OrcBrew files are EDN (Extensible Data Notation) files used by OrcPub
+and DungeonMastersVault for exporting D&D content.
+"""
+
+import logging
+
+logger = logging.getLogger(__name__)
